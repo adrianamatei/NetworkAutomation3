fefc9c8ca612bbc64c1585341b63f48cbf11c282
ionelaamatei2004 <ionelaamatei2004@gmail.com>
2025-09-04 23:13:44 +0300

    sedinta 4 septembrie final

--- p2_module 11/event.py
+++ p2_module 11/event.py
@@ -1,13 +1,16 @@
-import threading
-import time
+import threading, time
+
 event = threading.Event()
+
 def waiter():
-    print("waiting for event to be set ")
-    event.wait()
-    print("event detected, continuing")
+     print("Waiting for event to be set...")
+     event.wait()
+     print("Event detected, continuing")
+
 def setter():
-    time.sleep(2)
-    print("setting event")
-    event.set()
+     time.sleep(2)
+     print("Setting event")
+     event.set()
+
 threading.Thread(target=waiter).start()
 threading.Thread(target=setter).start()
--- p2_module 11/semaphores.py
+++ p2_module 11/semaphores.py
@@ -1,11 +1,13 @@
-import threading
-import time
 
-sem=threading.Semaphore(2)
+import threading, time
+
+sem = threading.Semaphore(2)  # max 2 threads allowed at once
+
 def worker(i):
-    with sem:
-        print(f"Thread {i} entered ")
-        time.sleep(2)
-        print(f"Thread {i} exiting ")
+     with sem:
+         print(f"Thread {i} entered")
+         time.sleep(2)
+         print(f"Thread {i} exiting")
+
 for i in range(5):
-    threading.Thread(target=worker,args=(i,)).start()
+     threading.Thread(target=worker, args=(i,)).start()
