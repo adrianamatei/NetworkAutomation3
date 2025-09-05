066f22c9bc3090184173ef35e1fd87e3bf6f2faa
ionelaamatei2004 <ionelaamatei2004@gmail.com>
2025-09-05 15:29:41 +0300

    Cod complet functional 4 septembrie

--- p2_module8/ex2.py
+++ p2_module8/ex2.py
@@ -1,7 +1,7 @@
 #ex2
 # check if second interface has IP and if not set IP
 # Silviu
-import re
+'''import re
 import subprocess
 import time
 
@@ -84,3 +84,32 @@ try:
     print(rezzy)
 except Exception as e:
     print('ip was tried to be given, though somehow failed' + str(e))
+'''
+import subprocess
+import re
+
+def get_interface_ip(iface):
+    result = subprocess.run(
+        ["ip", "-4", "addr", "show", iface],
+        capture_output=True,
+        text=True
+    )
+    match = re.search(r"inet (\d+.\d+.\d+.\d+)/\d+", result.stdout)
+    if match:
+        return match.group(1)
+    return None
+
+def set_interface_ip(iface, ip, mask="24"):
+    subprocess.run(["sudo", "ip", "addr", "flush", "dev", iface])  # curăță IP-urile vechi
+    subprocess.run(["sudo", "ip", "addr", "add", f"{ip}/{mask}", "dev", iface])
+    subprocess.run(["sudo", "ip", "link", "set", iface, "up"])
+
+if __name__ == "main":
+    iface = "ens4"
+    ip = get_interface_ip(iface)
+    if ip:
+        print(f"{iface} already has IP: {ip}")
+    else:
+        new_ip = "192.168.200.1"
+        set_interface_ip(iface, new_ip)
+        print(f"Set {new_ip}/24 on {iface}")
