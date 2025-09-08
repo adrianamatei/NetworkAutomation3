9e4741fc0446bbcda5778efefc1d17a1bc3cc8ff
ionelaamatei2004 <ionelaamatei2004@gmail.com>
2025-09-08 21:13:29 +0300

    Cod complet functional 8 septembrie

---
+++ lib/connectors/rest_con.py
@@ -0,0 +1,28 @@
+from requests.auth import HTTPBasicAuth
+from unicon.plugins.generic.service_statements import username, password
+import requests
+
+class RESTConnector:
+    def __init__(self,ip,port):
+        self.ip = ip
+        self.port = port
+        self.username = username
+        self.password = password
+        self._auth= None
+        self._session = None
+        self._headers = None
+        self._url=None
+
+    def connect(self):
+        self._auth=HTTPBasicAuth(username=self.username,password=self.password)
+        self._headers={
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+
+
+        }
+        self._url=f'https://{self.ip}:{self.port}'
+        response=requests.get(url=self._url,auth=self._auth,headers=self._headers,verify=False)
+        print(response.status_code)
+        print(response.text)
+
--- p2_module12/pyats_example.py
+++ p2_module12/pyats_example.py
@@ -1,46 +1,57 @@
 import asyncio
-from queue import Queue
-from multiprocessing import Queue
-from pyats import aetest, topology
 import subprocess
-from pyats.datastructures import AttrDict
-import sys
+import ipaddress
+import multiprocessing
+import time
 
+from pyats import aetest, topology
+import yaml
+
+from lib.connectors.rest_con import RESTConnector
 from lib.connectors.telnet_con import TelnetConnection
+import p2_module12.ssh_config
+from p2_module12 import ssh_config
 
-# from lib.connectors.ssh_conn import SshConnection
-# from lib.connectors.telnet_con import TelnetConnection
 
-obj = AttrDict()
-print(sys.path)
-
+# with open("testbed.yaml", "r") as yaml_file:
+#     data = yaml.load(yaml_file)
+#     print(data)
 
 class CommonSetup(aetest.CommonSetup):
     @aetest.subsection
     def load_testbed(self, steps):
-        with steps.start('Load testbed'):
+        with steps.start("Load testbed"):
             self.tb = topology.loader.load('new_testbed.yaml')
+            # dev=self.tb.devices.IOU1
+            # dev=self.tb.devices['IOU1']
+            # print(self.tb)
             self.parent.parameters.update(tb=self.tb)
+
 
     @aetest.subsection
     def bring_up_server_interface(self, steps):
         server = self.tb.devices['UbuntuServer']
-        for intf_name, intf in server.interfaces.items():
-            # intf = server.interfaces[interface]
-            with steps.start(f'Bring up interface {intf_name}'):
-                subprocess.run(['sudo', 'ip', 'addr', 'add', f'{intf.ipv4}', 'dev', f'{intf_name}'])
-                subprocess.run(['sudo', 'ip', 'link', 'set', 'dev', f'{intf_name}', 'up'])
+        for interface in server.interfaces:
+            intf = server.interfaces[interface]
+            with steps.start("Bring up interface {interface.name}"):
+                subprocess.run(['sudo', 'ip', 'addr', 'add', f'{intf.ipv4}', 'dev', f'{interface}'])
+                subprocess.run(['sudo', 'ip', 'link', 'set', 'dev', f'{interface}', 'up'])
 
-        with steps.start('Add routes'):
+        with steps.start("Add routes"):
             for device in self.tb.devices:
-                if self.tb.devices[device].type != 'router':
+                if self.tb.devices[device].type!='router':
                     continue
-                gateway = self.tb.devices[device].interfaces['initial'].ipv4.ip.compressed
+                gateway = self.tb.devices[device].interfaces['initial'].ipv4.compressed
                 for interface in self.tb.devices[device].interfaces:
-                    if self.tb.devices[device].interfaces[interface].link.name == 'management':
+                    if self.tb.devices[device].interfaces[interface].link.name=='management':
                         continue
                     subnet = self.tb.devices[device].interfaces[interface].ipv4.network.compressed
-                    subprocess.run(['sudo', 'ip', 'route', 'add', f'{subnet}', 'via', f'{gateway}'])
+                    print(subnet)
+
+                    subprocess.run(
+                        ['sudo', 'ip', 'route', 'add', f'{subnet}', 'via', f'{gateway}', 'metric'])
+
+                #print(device)
 
     @aetest.subsection
     def bring_up_server_interface(self, steps):
@@ -53,27 +64,71 @@ class CommonSetup(aetest.CommonSetup):
                     if self.tb.devices[device].interfaces[interface].link.name !='management':
                         continue
 
+                    intf_obj=self.tb.devices[device].interfaces[interface]
                     conn_class=self.tb.devices[device].connections.get('telnet',{}).get('class',None)
                     assert conn_class,'No connection for device {}'.format(device)
-                    ip=self.tb.devices[device].connections.telnet.ip.compressed
-                    port=self.tb.devices[device].connections.telnet.port
-                    conn:TelnetConnection = conn_class(ip, port)
+                    ip = self.tb.devices[device].connections.telnet.ip.compressed
+                    port = self.tb.devices[device].connections.telnet.port
+                    q=multiprocessing.Queue()
+
+                    commands=ssh_config.commands
+                    formatted_commands=list(map(
+                        lambda s: s.format(
+                            interface=interface,
+                            ip=intf_obj.ipv4.ip.compressed,
+                            sm=intf_obj.ipv4.netmask.exploded,
+                            hostname=device,
+                            domain=self.tb.devices[device].custom.get('domain',''),
+                            username=self.tb.devices[device].connections.telnet.credentials.login.username,
+                            password=self.tb.devices[device].connections.telnet.credentials.login.password,
+
+                        ),
+                        commands
+                    ))
+
+                    conn: TelnetConnection = conn_class(ip, port)
+
 
                     async def setup():
                         await conn.connect()
-                        await conn.configure()
+                        time.sleep(1)
+                        await conn.execute_commends(formatted_commands, '#')
                     asyncio.run(setup())
+    @aetest.subsection
+    def connect_via_rest(self,steps):
+        for device in self.tb.devices:
+            if self.tb.devices[device].type != 'router':
+                continue
+            if 'rest' not in self.tb.devices[device].connections:
+                continue
+
+
+            for interface in self.tb.devices[device].interfaces:
+                if self.tb.devices[device].interfaces[interface].link.name !='management':
+                    continue
+                conn_class: RESTConnector=self.tb.devices[device].connections['rest']['class']
+                conn_data=self.tb.devices[device].connections['rest']
+                conn_obj=conn_class(
+                    ip=conn_data.ip.compressed,
+                    port=conn_data['port'],
+                    username=conn_data['username'],
+                    password=conn_data['password'],
+                )
 
 
 
 
 
 
+class ConfigureInterfaces(aetest.Testcase):
 
-
-
-
+    @aetest.setup
+    def configure(self):
+        tb = self.parent.parameters['tb']
+        conn = tb.devices['IOU1'].connections.telnet['class']
+        print(conn)
 
 
 if __name__ == '__main__':
     aetest.main()
+
