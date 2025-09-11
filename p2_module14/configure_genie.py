5754aa8c1684ecf9ffd171e5ab60e545bb64888d
ionelaamatei2004 <ionelaamatei2004@gmail.com>
2025-09-11 19:10:41 +0300

    genie

--- p2_module14/configure_genie.py
+++ p2_module14/configure_genie.py
@@ -1,1 +1,24 @@
 import genie
+from pyats import aetest, topology
+from genie.libs.conf.interface.iosxe import Interface
+from pyats.topology import Testbed
+
+
+class CommonSetup(aetest.CommonSetup):
+    @aetest.subsection
+    def load_testbed(self, steps):
+        with steps.start("Load testbed"):
+            tb = topology.loader.load('new_testbed.yaml')
+            self.parent.parameters.update(tb=tb)
+
+
+class ConfigureGenie(aetest.Testcase):
+    @aetest.setup
+    def test1(self, steps):
+        tb: Testbed = self.parent.parameters.get("tb")
+        dev = tb.devices.RouterCSR
+        dev.connect(log_stdout=True)
+        print(dev)
+
+if __name__ == '__main__':
+    aetest.main()
