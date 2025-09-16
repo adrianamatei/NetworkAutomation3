ddc57e13a23e5cb7102b9cd1e6f512610923c5ac
ionelaamatei2004 <ionelaamatei2004@gmail.com>
2025-09-16 20:38:51 +0300

    Corectare testbed

--- p2_module15/initial_config_ftd.py
+++ p2_module15/initial_config_ftd.py
@@ -38,92 +38,101 @@ class ConfigureFDMManagement(aetest.Testcase):
 
                     async def setup():
                         await conn.connect()
-                        time.sleep(1)
-                        conn.write('')
-                        time.sleep(2)
-                        out = await conn.read(n=1000)
+                        await asyncio.sleep(1)
+
+                        conn.write("\n")
+                        await asyncio.sleep(2)
+                        out = await conn.read(n=2000)
                         print(out)
+
                         result = re.search(r'^\s*(?P<login>firepower login:)', out)
                         if not result:
                             step.skipped(reason='Configuration not required')
 
-                        if result.group('login'):
-                            conn.write('admin')
-                            time.sleep(1)
-                            conn.write('Admin123')
-                            time.sleep(1)
+                        if result and result.group('login'):
+                            conn.write("admin\n")
+                            await asyncio.sleep(1)
+                            conn.write("Admin123\n")
+                            await asyncio.sleep(1)
+                            out = await conn.read(n=2000)
 
-                        out = await conn.read(n=1000)
-                        if 'EULA:' in out:
-                            conn.write('\n')
+                        # EULA
+                        if "EULA:" in out:
+                            conn.write("\n")
+                            await asyncio.sleep(1)
 
                             while True:
-                                time.sleep(1)
-                                out = await conn.read(n=1000)
-                                if '--More--' in out:
-                                    conn.write(' ')
-                                elif 'EULA:' in out:
-                                    conn.write('\n')
-                                    time.sleep(1)
-                                    out = await conn.read(n=1000)
+                                out = await conn.read(n=2000)
+                                if "--More--" in out:
+                                    conn.write(" ")
+                                elif "AGREE" in out:
+                                    conn.write("YES\n")
+                                    await asyncio.sleep(1)
+                                    out = await conn.read(n=2000)
                                     break
                                 else:
-                                    print('no str found in eula')
+                                    break
 
-                        if 'password:' in out:
-                            conn.write(self.tb.devices[device].credentials.default.password.plaintext)
-                            time.sleep(1)
-                            out = await conn.read(n=1000)
-                            if 'password:' in out:
-                                conn.write(self.tb.devices[device].credentials.default.password.plaintext)
-                                time.sleep(1)
-                                out = await conn.read(n=1000)
+                        # New password
+                        if "Enter new password:" in out:
+                            pw = self.tb.devices[device].credentials.default.password.plaintext
+                            conn.write(pw + "\n")
+                            await asyncio.sleep(1)
+                            out = await conn.read(n=2000)
 
-                        if 'IPv4? (y/n) [y]:' in out:
-                            conn.write('')
-                            time.sleep(1)
-                            out = await conn.read(n=1000)
+                        if "Confirm new password:" in out:
+                            pw = self.tb.devices[device].credentials.default.password.plaintext
+                            conn.write(pw + "\n")
+                            await asyncio.sleep(1)
+                            out = await conn.read(n=2000)
 
-                        if 'IPv6? (y/n) [n]:' in out:
-                            conn.write('')
-                            time.sleep(1)
-                            out = await conn.read(n=1000)
+                        # IPv4
+                        if "IPv4? (y/n) [y]:" in out:
+                            conn.write("\n")
+                            await asyncio.sleep(1)
+                            out = await conn.read(n=2000)
 
-                        if '[manual]:' in out:
-                            conn.write('')
-                            time.sleep(1)
-                            out = await conn.read(n=1000)
+                        if "IPv6? (y/n) [n]:" in out:
+                            conn.write("\n")
+                            await asyncio.sleep(1)
+                            out = await conn.read(n=2000)
 
-                        if '[192.168.45.45]:' in out:
-                            conn.write(intf_obj.ipv4.ip.compressed)
-                            time.sleep(1)
-                            out = await conn.read(n=1000)
+                        if "(dhcp/manual) [manual]:" in out:
+                            conn.write("\n")
+                            await asyncio.sleep(1)
+                            out = await conn.read(n=2000)
 
-                        if '[255.255.255.0]:' in out:
-                            conn.write(intf_obj.ipv4.netmask.exploded)
-                            time.sleep(1)
-                            out = await conn.read(n=1000)
+                        if "Enter an IPv4 address" in out:
+                            conn.write(intf_obj.ipv4.ip.compressed + "\n")
+                            await asyncio.sleep(1)
+                            out = await conn.read(n=2000)
 
-                        if '[192.168.45.1]:' in out:
-                            conn.write((intf_obj.ipv4.ip + 1).compressed)
-                            time.sleep(1)
-                            out = await conn.read(n=1000)
+                        if "Enter an IPv4 netmask" in out:
+                            conn.write(intf_obj.ipv4.netmask.exploded + "\n")
+                            await asyncio.sleep(1)
+                            out = await conn.read(n=2000)
 
-                        if '::35]:' in out:
-                            conn.write('')
-                            time.sleep(1)
-                            out = await conn.read(n=1000)
+                        if "default gateway" in out:
+                            conn.write((intf_obj.ipv4.ip + 1).compressed + "\n")
+                            await asyncio.sleep(1)
+                            out = await conn.read(n=2000)
 
-                        if "'none' []:" in out:
-                            conn.write('')
-                            time.sleep(1)
-                            out = await conn.read(n=1000)
+                        if "DNS servers" in out:
+                            conn.write("\n")
+                            await asyncio.sleep(1)
+                            out = await conn.read(n=2000)
+
+                        if "search domains" in out:
+                            conn.write("\n")
+                            await asyncio.sleep(1)
+                            out = await conn.read(n=2000)
 
                         if "locally? (yes/no) [yes]:" in out:
-                            conn.write('')
-                            time.sleep(1)
-                            out = await conn.read(n=1000)
+                            conn.write("\n")
+                            await asyncio.sleep(1)
+                            out = await conn.read(n=2000)
 
+                        print("Final output:", out)
 
                     asyncio.run(setup())
 
