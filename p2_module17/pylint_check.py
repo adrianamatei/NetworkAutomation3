8b9917a6823f828845445820a33c4787d95c7b8f
ionelaamatei2004 <ionelaamatei2004@gmail.com>
2025-09-23 18:57:48 +0300

    Totul pana la modulul 17

--- p2_module17/pylint_check.py
+++ p2_module17/pylint_check.py
@@ -4,5 +4,5 @@ import sys
 print(sys.path)
 
 
-args = [ '--rcfile=pylintrc', 'modul16/configure_ftd_int.py']
+args = [ '--rcfile=pylintrc', 'p2_module16/configure_ftd_int_experiment.py']
 pylint.run_pylint(args)
